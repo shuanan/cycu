01d84f398c0b6f7cf9fc3b93ad0191710ac34618
shuanan <samualjoey@gmail.com>
2018-04-12 11:16:24 +0800

    cycle

--- uidesign/app/src/main/java/com/gmail/samualjoey/uidesign/MainActivity.java
+++ uidesign/app/src/main/java/com/gmail/samualjoey/uidesign/MainActivity.java
@@ -2,6 +2,7 @@ package com.gmail.samualjoey.uidesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookkeeping);
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("test", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("test", "onResume");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("test", "onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("test", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("test", "onDestroy");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("test", "onStart");
+    }
+
 }
